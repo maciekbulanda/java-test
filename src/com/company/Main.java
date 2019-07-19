ae631b3becca22af6b114893a5cb0d9f30d4a5b0
Maciej Bulanda <bma@post.pl>
2019-07-19 08:26:30 +0200

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("main thread");
+        System.out.println("main threads");
         new Thread((Runnable) () -> System.out.println("test:" + Thread.currentThread().getName()),"my new thread").start();
     }
 }
