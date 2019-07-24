591b8aaff823518d734ec4baf584ac285bd7632a
Maciej Bulanda <bma@post.pl>
2019-07-24 22:00:15 +0200

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,19 @@
 package com.company;
 import java.lang.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("main thread");
-        MyThread t = new MyThread();
-        t.start();
-        String text = scanner.nextLine();
-        t.stop = true;
-        System.out.println(text);
-        System.out.println("end.....");
+        int num;
+        List<Integer> lista = new ArrayList<>();
+        while ((num = scanner.nextInt()) != 0) {
+            lista.add(num);
+        }
+        Stream<Integer> str = lista.stream();
+        str.distinct().sorted().forEach(System.out::println);
     }
 }
